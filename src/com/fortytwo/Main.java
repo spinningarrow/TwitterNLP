f7e475b61e3a7d18729b1ad5753504024113a19d
Sahil Bajaj <sahil29@gmail.com>
2013-11-09 19:21:21 +0800

    Initial commit

---
+++ src/com/fortytwo/Main.java
@@ -0,0 +1,14 @@
+package com.fortytwo;
+
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.trees.LeftHeadFinder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Gagoman");
+
+//        LexicalizedParser lp = LexicalizedParser
+//                .loadModelFromZip("edu/stanford/nlp/models/lexparser/englishPCFG.caseless.ser.gz", "englishPCFG.caseless.ser.gz");
+    }
+}
