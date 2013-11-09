52ae9c5b32474a2a4ed3cf213e5aa4260dddf08d
Arnav Kumar <arnavkumar24@hotmail.com>
2013-11-09 22:26:05 +0800

    Changed test sentence again

--- src/com/fortytwo/ParserDemo.java
+++ src/com/fortytwo/ParserDemo.java
@@ -72,9 +72,9 @@ class ParserDemo {
     public static void demoAPI(LexicalizedParser lp) {
         // This option shows parsing a list of correctly tokenized words
 //        String[] sent = {"This", "is", "an", "easy", "sentence", "."};
-        String sentence = "The iPad Air is lovely, light and awesome";
+//        String sentence = "The iPad Air is lovely, light and awesome";
 //        String sentence = "iPad Air you skinny bitch!!";
-//        String sentence = "I want the iPad Air";
+        String sentence = "I want the iPad Air";
 //        String sentence = "I LOVE the iPad Air ad.";
         String query = "iPad Air";
 
