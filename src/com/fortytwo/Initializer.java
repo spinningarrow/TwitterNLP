90708572cdb84b16fd0ddb92ebe10bc3846cf5b8
Arnav Kumar <arnavkumar24@hotmail.com>
2013-11-10 23:05:01 +0800

    Initializer works...

---
+++ src/com/fortytwo/Initializer.java
@@ -0,0 +1,163 @@
+package com.fortytwo;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.process.WhitespaceTokenizer;
+import edu.stanford.nlp.trees.Tree;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Radzinzki
+ * Date: 10/11/13
+ * Time: 4:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Initializer {
+    public static void main (String []args)
+    {
+        //createDatabase("data/database.db");
+//       readAndStoreTweets("data/database.db", "data/plaintextSet1.txt", "data/taggedSet1.txt");
+        printData("data/database.db");
+    }
+
+    private static void readAndStoreTweets(String databaseFile, String plaintextFile, String taggedFile)
+    {
+        try {
+            BufferedReader br1 = new BufferedReader(new FileReader(plaintextFile));
+            BufferedReader br2 = new BufferedReader(new FileReader(taggedFile));
+            String plain_line  = null;
+            String tagged_line = null;
+
+            while ((plain_line = br1.readLine()) != null)
+            {
+                tagged_line = br2.readLine();
+                insertTweet(databaseFile, plain_line, tagged_line);
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private static void printData(String filename)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:" + filename);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM TWEETS;" );
+            while ( rs.next() ) {
+                String text = rs.getString("text");
+                Tree parseTree = (Tree) Serializer.deserialize(rs.getBytes("parsetree"));
+                System.out.println(text);
+                parseTree.pennPrint();
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+    public static void createDatabase(String filename)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:" + filename);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE TWEETS " +
+                    "(TEXT           TEXT    NOT NULL, " +
+                    " PARSETREE      BLOB     NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+    private static void insertTweet (String databaseFile, String plaintextTweet, String taggedTweet)
+    {
+        // parse it
+        Tree parsedSentence = parseSentence("models/SerializedModel4", taggedTweet);
+
+        // store it
+        try {
+            store(databaseFile, plaintextTweet, Serializer.serialize(parsedSentence));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+    private static void store (String filename, String sentence, byte[] parseTree)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:" + filename);
+            c.setAutoCommit(false);
+
+            //stmt = c.createStatement();
+//            String sql = "INSERT INTO TWEETS (TEXT,PARSETREE) " +
+//                    "VALUES (" + sentence + "," + parseTree + " );";
+//            stmt.executeUpdate(sql);
+
+            PreparedStatement ps = c.prepareStatement("INSERT INTO TWEETS (TEXT,PARSETREE) " +
+                    "VALUES (?,? );");
+            ps.setString(1, sentence);
+            ps.setBytes(2, parseTree);
+            ps.executeUpdate();
+            ps.close();
+//            stmt.close();
+            c.commit();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+
+    public static Tree parseSentence (String modelFile, String sentence)
+    {
+        LexicalizedParser lp = LexicalizedParser.loadModel(modelFile);
+        DocumentPreprocessor dp = new DocumentPreprocessor(new StringReader(sentence));
+//        DocumentPreprocessor dp = new DocumentPreprocessor("data/Sample1Tweet.txt");
+        TokenizerFactory<CoreLabel> tf = WhitespaceTokenizer.newCoreLabelTokenizerFactory("");
+
+        dp.setSentenceDelimiter("\n");
+        dp.setTagDelimiter("_");
+        dp.setTokenizerFactory(tf);
+
+
+        Tree parseTree = lp.parse(sentence);
+        return parseTree;
+    }
+}
---
+++ src/com/fortytwo/Serializer.java
@@ -0,0 +1,28 @@
+package com.fortytwo;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Radzinzki
+ * Date: 10/11/13
+ * Time: 8:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Serializer {
+    public static byte[] serialize(Object obj) throws IOException {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        ObjectOutputStream o = new ObjectOutputStream(b);
+        o.writeObject(obj);
+        return b.toByteArray();
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream b = new ByteArrayInputStream(bytes);
+        ObjectInputStream o = new ObjectInputStream(b);
+        return o.readObject();
+    }
+}
